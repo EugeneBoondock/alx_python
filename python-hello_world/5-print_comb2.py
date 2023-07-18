d67e14cc2b64170b668ec25860f66ebdaa9da3b4
Eugene Ncube <philosncube@gmail.com>
2023-07-18 23:20:59 +0200

    2 digit number

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    if i < 10:
-        print("{}{}".format(0, i), end=", ")
+    if i < 99:
+        print("{:02}, ".format(i), end="")
     else:
-        print("{}".format(i), end=", ")
+        print("{:02}".format(i))
