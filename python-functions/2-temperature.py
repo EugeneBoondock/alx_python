4cec8c4be2ce81cc0f772c392223eaf35dc08ef6
Eugene Ncube <philosncube@gmail.com>
2023-07-20 14:34:37 +0200

    another fix

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,4 +2,7 @@
 
 def convert_to_celsius(fahrenheit):
     celcius = (5/9) * (fahrenheit - 32)
-    return celcius
+    if celcius < 0:
+        return ("{:2f}".format(celcius))
+    else:
+        return celcius
