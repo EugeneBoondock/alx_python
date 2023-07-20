5477355672edef541dc1881e95ddbca1561bc1e2
Eugene Ncube <philosncube@gmail.com>
2023-07-20 15:02:28 +0200

    fix mistake

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -3,8 +3,3 @@
 def reverse_string(string):
     rev = string[::-1]
     return rev
-
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World!"))
