c516caf9faa25e7b38a9e3a4a4473a087e5e1a87
Eugene Ncube <philosncube@gmail.com>
2023-07-20 21:39:05 +0200

    done

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -4,6 +4,7 @@ def validate_password(password):
     if len(password) >= 8:
         has_uppercase = False
         has_lowercase = False
+        has_digit = False
 
         for char in password:
             if char.isspace():
@@ -12,6 +13,8 @@ def validate_password(password):
                 has_uppercase = True
             if char.islower():
                 has_lowercase = True
+            if char.isdigit():
+                has_digit = True
 
         return has_uppercase and has_lowercase
     else:
