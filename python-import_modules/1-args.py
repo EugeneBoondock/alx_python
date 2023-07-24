0a727ccf7368846058081a5e98c3f5cccb09234d
Eugene Ncube <philosncube@gmail.com>
2023-07-24 21:58:18 +0200

    fix

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
 
     if argnum == 0:
         print("{} arguments.".format(argnum))
+    elif argnum == 1:
+        print("{} argument:".format(argnum))
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
     else:
-        print("{} argument(s):".format(argnum))
+        print("{} arguments:".format(argnum))
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
