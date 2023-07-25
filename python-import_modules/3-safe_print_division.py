c964fc6261f5458708150d2cbd8b143bb9dde75a
Eugene Ncube <philosncube@gmail.com>
2023-07-25 15:53:59 +0200

    fix

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,11 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        result = int(a/b)
+        result = a / b
     except ZeroDivisionError:
-        a or b == 0
-        print("{} / {}= None".format(a, b))
+        print("{} / {} = None".format(a, b))
     finally:
-        a or b != 0
-        print("Inside result: {}".format(float(a/b)))
-        print("{} / {} = {}".format(a, b, float(a/b)))
+        print("Inside result: {}".format(result))
+        print("{} / {} = {}".format(a, b, result))
