b8318dd474bebd5098f01255aefbcda8a433985d
Eugene Ncube <philosncube@gmail.com>
2023-07-25 16:02:05 +0200

    fix2

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,6 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
+        print("Inside result: None")
         print("{} / {} = None".format(a, b))
         result = None
     finally:
