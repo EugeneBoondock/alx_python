6df4812ac4fd41729443bba5ad4726d3a9614559
Eugene Ncube <philosncube@gmail.com>
2023-07-28 01:07:44 +0200

    fixed again

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
     for length in result:
         length = len(result)
         first = result[0]
-        if result == 0:
+        if len(result) == 0:
             first = None
         return length, first
