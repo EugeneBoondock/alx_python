0c7b2cad6fc8a18c5813d073714eb52c1f167301
Eugene Ncube <philosncube@gmail.com>
2023-07-28 00:53:41 +0200

    fixed

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,9 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            elem = row[i]
-            print("{:d}".format(elem), end=" ")
-            if i == len(row) - 1:
-                print("{:d}".format(elem), end="")
+        for i, elem in enumerate(row):
+            print("{:d}".format(elem), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
         print()
+
