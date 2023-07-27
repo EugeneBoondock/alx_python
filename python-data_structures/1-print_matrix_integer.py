eb2376b8f627ef564dd1a629517c7a29fedf661a
Eugene Ncube <philosncube@gmail.com>
2023-07-28 00:48:16 +0200

    fix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,6 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elem in row:
+        for i in range(len(row)):
+            elem = row[i]
             print("{:d}".format(elem), end=" ")
+            if i == len(row) - 1:
+                print("{:d}".format(elem), end="")
         print()
