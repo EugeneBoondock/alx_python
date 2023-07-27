06d53a56cf70ac61c78b4f4eaac541e7a990d416
EugeneBoondock <philosncube@gmail.com>
2023-07-27 20:20:59 +0000

    Fixed no c in string
    None

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = str(my_string)
+    result = ""
     for char in my_string:
-        if char == my_string[0]:
-            if char == 'c' or char == 'C':
-                return("{}".format(my_string[1:]))
-        else:
-            return my_string
+        if char.lower() != 'c':
+            result += char
+    return result
