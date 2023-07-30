ed0989bcd29761fa6fdd3782ca5b9857a81c8a90
Eugene Ncube <philosncube@gmail.com>
2023-07-30 17:05:57 +0200

    common elements2

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-
+    common_set = set()
     for x in set_1:
         for y in set_2:
             if x == y:
-                return("{}".format(x, y))
+                common_set.add(x)
+    return list(common_set)
