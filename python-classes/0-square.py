79f9daa056a6b943f97a2452899f11bae3f3fe3b
EugeneBoondock <philosncube@gmail.com>
2023-08-03 08:15:11 +0000

    Fix square 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a square by a private instance
-instantiation is optional.
+This module defines a square by a private instance.
+
 """
 class Square: 
      """defines class and instantiates private instance attribute size.""" 
