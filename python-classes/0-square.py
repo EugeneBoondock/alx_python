58716223d68e2c51df0e04c86d55e206076eeba7
EugeneBoondock <philosncube@gmail.com>
2023-08-03 07:34:49 +0000

    Fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,22 +1,9 @@
-#!/usr/bin/python3
-"""
-
-This module Initializes a square instance
-with a given size
-
-"""
-
-class Square:
-    """
-    class named Square
-    """
-    def __init__(self, size):
-        """
-        Initialize a Square instance with the given size.
-
-        Parameters:
-        size (int): The side length of the square.
-        """
-        self.__size = size
-
-my_square = Square()
+#!/usr/bin/python3 
+ """creates class Square with private instance attribute size""" 
+  
+  
+ class Square: 
+     """defines class and instantiates private instance attribute size.""" 
+  
+     def __init__(self, size): 
+         self.__size = size
