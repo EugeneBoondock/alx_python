04014a71da3a754904856af5ff9e5591cab7d567
EugeneBoondock <philosncube@gmail.com>
2023-08-03 08:00:50 +0000

    Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines a square by a private instance
+instantiation is optional.
+"""
+class Square:
+    """
+    Represents a square with a given size.
+
+    Attributes:
+        __size (int): The side length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is a negative integer.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance with the given size.
+
+        Parameters:
+            size (int, optional): The side length of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
