b801b6c9cfdc3ac85f6128db08b127a78cc0b871
Eugene Ncube <philosncube@gmail.com>
2023-08-08 01:37:12 +0200

    BaseGeo

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+#!//usr/bin/python3
+"""
+Empty BaseGeometry Module
+"""
+class BaseGeometry:
+    """
+    Raises exceptions and there are values involved
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = str(name)
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        
+        self.value = value
