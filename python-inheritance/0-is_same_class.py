66c946b7845386e551e30c732b19d0324d1ec6d1
Eugene Ncube <philosncube@gmail.com>
2023-08-07 03:04:49 +0200

    fix

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,4 +3,7 @@
 This module checks if an object is an instance of a class using isinstance
 """
 def is_same_class(obj, a_class):
+    """
+    This function defines an object and a class
+    """
     return isinstance(obj, a_class)
