afc63fb9ac387889ff7558d32dcf1e5d3a615480
Eugene Ncube <philosncube@gmail.com>
2023-08-08 01:52:33 +0200

    rectangle inheritance

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,28 @@
+#!//usr/bin/python3
+"""
+Empty BaseGeometry Module
+"""
+class BaseGeometry:
+    """
+    Raises exceptions and there are values involved
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = str(name)
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+        self.value = value
+
+class Rectangle(BaseGeometry):
+    
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def integer_validator(self, width, height):
+        return super().integer_validator(width, height)
