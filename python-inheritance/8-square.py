5280adcbd03e445df9b3ab5504f0d5dbedb23882
Eugene Ncube <philosncube@gmail.com>
2023-08-08 03:19:42 +0200

    fix5

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-"""
-Empty BaseGeometry Module
-"""
+'''creating an empty class script'''
 
 
-class BaseGeometry:
-    """
-    Raises exceptions and there are values involved
-    """
+class BaseGeometryMetaClass(type):
+    '''
+    creating the meta Class to remove unwanted subclasses.
+    '''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass = BaseGeometryMetaClass):
+    '''empty class created'''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    pass
+
     def area(self, width, height):
         raise Exception("area() is not implemented")
 
