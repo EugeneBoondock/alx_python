5d99e7329a093c7d7c22511b775f5f6768ea1b73
Eugene Ncube <philosncube@gmail.com>
2023-08-08 23:31:08 +0200

    fix with docs

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,33 +1,57 @@
 #!/usr/bin/python3
 import unittest
 """
-This is a new module
+A module for class Base
 """
 
 
 class Base:
     """
-    This class is the base of all other classes in the project.
+    The base class for managing id attribute in all other classes.
+
+    Attributes:
+    __nb_objects (int): Private class attribute to track objects.
+
+    Methods:
+        __init__(self, id=None): Class constructor to initialize an instance.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initializes an instance of the Base class.
+
+        Args:
+            id (int, optional): If provided, assigns the id attribute with this value.
+            If not provided, increments __nb_objects and assigns the new value to id.
+        """
         if id is not None:
             self.id = int(id)
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 """
-A unittest class
+Module for class Test
 """
 
 
 class TestBase(unittest.TestCase):
+    """
+    Unit tests for the Base class.
+    """
+
     def test_create_instance_with_id(self):
+        """
+        Test creating an instance of Base with a specific id.
+        """
         b = Base(10)
         self.assertEqual(b.id, 10)
 
     def test_create_instance_without_id(self):
+        """
+        Test creating an instance of Base without specifying an id.
+        """
         b = Base()
         self.assertEqual(b.id, 1)
 
