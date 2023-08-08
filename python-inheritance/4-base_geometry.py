d341936202d84c1ed555308a12330b363f43c0a9
Eugene Ncube <philosncube@gmail.com>
2023-08-08 03:13:16 +0200

    another fix

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,10 +1,22 @@
 #!//usr/bin/python3
-"""
-Empty BaseGeometry Module
-"""
-class BaseGeometry:
-    """
-    empty
-    """
+'''creating an empty class script'''
+class BaseGeometryMetaClass(type):
+    '''
+    creating the meta Class to remove unwanted subclasses.
+    '''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass = BaseGeometryMetaClass):
+    '''empty class created'''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    pass
+
     def area(self):
         raise Exception("area() is not implemented")
