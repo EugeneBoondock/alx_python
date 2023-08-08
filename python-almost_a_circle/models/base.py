ef73d278e71ab7de34da4b80afe51bccb26b5e36
Eugene Ncube <philosncube@gmail.com>
2023-08-08 23:27:42 +0200

    confusion

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+import unittest
 """
-This module is a Base module
+This is a new module
 """
-
-
 class Base:
     """
-    This function increments and assigns stuff
+    This class is the base of all other classes in the project.
     """
+    __nb_objects = 0
+
     def __init__(self, id=None):
-
-        __nb_objects = 0
-
-        if id is None:
+        if id is not None:
+            self.id = int(id)
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else:
-            self.id = int(id)
+
+class TestBase(unittest.TestCase):
+    def test_create_instance_with_id(self):
+        b = Base(10)
+        self.assertEqual(b.id, 10)
+
+    def test_create_instance_without_id(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+if __name__ == "__main__":
+    unittest.main()
