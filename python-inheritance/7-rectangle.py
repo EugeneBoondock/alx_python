bbcfe24a49b0aa35526e9096e54ed0ab8ec1fefb
Eugene Ncube <philosncube@gmail.com>
2023-08-08 03:18:12 +0200

    fix4

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""
-Empty BaseGeometry Module
-"""
-class BaseGeometry:
-    """
-    Raises exceptions and there are values involved
-    """
+'''creating an empty class script'''
+class BaseGeometryMetaClass(type):
+    '''
+    creating the meta Class to remove unwanted subclasses.
+    '''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass = BaseGeometryMetaClass):
+    '''empty class created'''
+    def __dir__(cls):
+        '''
+        function method creats a list of all attributes for the class and excludes the init_subclass.
+        '''
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    pass
+
     def area(self, width, height):
         raise Exception("area() is not implemented")
 
