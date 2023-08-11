bf08b33880ac555b468e3cb7a9b96534b2497355
Eugene Ncube <philosncube@gmail.com>
2023-08-11 03:00:48 +0200

    maybe fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -53,12 +53,12 @@ class Rectangle(Base):
             ValueError: If the new width value is not greater than 0.
         """
         if not isinstance(new_width, int):
-            raise TypeError(f"{new_width} must be an integer")
+            raise TypeError(f"[TypeError] width must be an integer")
 
         if new_width > 0:
             self.__width = new_width
         else:
-            raise ValueError(f"{new_width} must be > 0")
+            raise ValueError(f"[ValueError] width must be > 0")
 
     @property
     def height(self):
@@ -79,12 +79,12 @@ class Rectangle(Base):
             ValueError: If the new height value is not greater than 0.
         """
         if not isinstance(new_height, int):
-            raise TypeError(f"{new_height} must be an integer")
+            raise TypeError(f"[TypeError] height must be an integer")
 
         if new_height > 0:
             self.__height = new_height
         else:
-            raise ValueError(f"[ValueError] {new_height} must be > 0")
+            raise ValueError(f"[ValueError] height must be > 0")
 
     @property
     def x(self):
@@ -105,10 +105,10 @@ class Rectangle(Base):
             ValueError: If the new x-coordinate value is less than 0.
         """
         if not isinstance(new_x, int):
-            raise TypeError(f"{new_x} must be an integer")
+            raise TypeError(f"[TypeError] x must be an integer")
 
         if new_x < 0:
-            raise ValueError(f"{new_x} must be >= 0")
+            raise ValueError(f"[ValueError] x must be >= 0")
         else:
             self.__x = new_x
     
@@ -132,10 +132,10 @@ class Rectangle(Base):
         """
 
         if not isinstance(new_y, int):
-            raise TypeError(f"{new_y} must be an integer")
+            raise TypeError(f"[TypeError] y must be an integer")
         
         if new_y < 0:
-            raise ValueError(f"{new_y} must be >= 0")
+            raise ValueError(f"[ValueError] y must be >= 0")
         else:
             self.__y = new_y
 
