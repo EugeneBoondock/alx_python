d554de8381506f1349ff41e75527da318cae6cc7
Eugene Ncube <philosncube@gmail.com>
2023-08-11 02:41:08 +0200

    unittest

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """
 This module defines the Rectangle class, which is a subclass of the Base class.
 """
+import unittest
 from models.base import Base
 
 
@@ -137,3 +138,56 @@ class Rectangle(Base):
             raise ValueError(f"{new_y} must be >= 0")
         else:
             self.__y = new_y
+
+
+"""
+Unittest Module
+"""
+class TestRect(unittest.TestCase):
+    """
+    Unit tests for the Rectangle class.
+    """
+
+    def test_values_are_integers(self):
+        """
+        Test checking if values are int.
+        """
+        y = Rectangle(10, 5, 2, 4)
+        self.assertIsInstance(y.id, int)
+        self.assertIsInstance(y.width, int)
+        self.assertIsInstance(y.height, int)
+        self.assertIsInstance(y.x, int)
+        self.assertIsInstance(y.y, int)
+
+    def test_values_are_not_integers(self):
+        """
+        Test checking if values are not int.
+        """
+        with self.assertRaises(TypeError):
+            n = Rectangle('x', 'b', 'q', 'f')
+
+    def test_values_are_greater_than0(self):
+        """
+        Test checking if values are greater than 0.
+        """
+        g = Rectangle(1, 4, 6, 4)
+        self.assertGreater(g.id, 0)
+        self.assertGreater(g.width, 0)
+        self.assertGreater(g.height, 0)
+        self.assertGreater(g.x, 0)
+        self.assertGreater(g.y, 0)
+
+    def test_values_are_less_than0(self):
+        """
+        Test checking if values are less than 0.
+        """
+        l = Rectangle(-1, -6, -16, -7)
+        self.assertLess(l.id, 0)
+        self.assertLess(l.width, 0)
+        self.assertLess(l.height, 0)
+        self.assertLess(l.x, 0)
+        self.assertLess(l.y, 0)
+
+if __name__ == "__main__":
+    unittest.main()
+
