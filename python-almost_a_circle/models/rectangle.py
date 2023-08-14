0f9200dfdf2cb90b84a66f0eda1596deda97c103
Eugene Ncube <philosncube@gmail.com>
2023-08-14 16:14:40 +0200

    indent fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -57,21 +57,21 @@ class Rectangle(Base):
         """
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
     
-def update(self, *args):
-    """
-    Updates the attributes based on the number of arguments
-    """
-    if len(args) == 1:
-        self.id = args[0]
-    if len(args) == 2:
-        self.__width = args[1]
-    if len(args) == 3:
-        self.__height = args[2]
-    if len(args) == 4:
-        self.__x = args[3]
-    if len(args) == 5:
-        self.__y = args[4]
-    return args
+    def update(self, *args):
+        """
+        Updates the attributes based on the number of arguments
+        """
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.__width = args[1]
+        if len(args) == 3:
+            self.__height = args[2]
+        if len(args) == 4:
+            self.__x = args[3]
+        if len(args) == 5:
+            self.__y = args[4]
+        return args
 
     @property
     def width(self):
