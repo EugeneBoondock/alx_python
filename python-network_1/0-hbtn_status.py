ad32560d56bcbfc21627d445669540c6799ff3a8
Eugene Ncube <philosncube@gmail.com>
2023-08-16 01:36:54 +0200

    cheatcool

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """
-Sends request to alu website
+Script: network_status_checker.py
+A simple script to check the status of a website using the 'requests' library.
+
+Author: [Your Name]
+
+Usage:
+    Run this script using the command: python network_status_checker.py
 """
+
+
 import requests
 
+r = 'https://alu-intranet.hbtn.io/status'
 
-r = requests.get('https://alu-intranet.hbtn.io/status')
-print(requests.text)
+try:
+    response = requests.get(r)
+    response.raise_for_status()
+
+    print(f"Body response {r}:\n")
+    print(response.text)
+    
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while trying to access {r}: {e}")
