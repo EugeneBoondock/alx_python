349acf84222c46338b14d54d2b2ddf543b3588e7
Eugene Ncube <philosncube@gmail.com>
2023-08-16 01:40:12 +0200

    hotfix

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -18,8 +18,9 @@ try:
     response = requests.get(r)
     response.raise_for_status()
 
-    print(f"Body response {r}:\n")
-    print(response.text)
-    
+    print(f"Body response:\n")
+    print("- type: {}".format(type(r)))
+    print("content: {}".format(response.text))
+
 except requests.exceptions.RequestException as e:
     print(f"An error occurred while trying to access {r}: {e}")
