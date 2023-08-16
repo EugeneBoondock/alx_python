744b3895407750b7d835260e99874a0ec9a05c7d
Eugene Ncube <philosncube@gmail.com>
2023-08-16 17:27:06 +0200

    status codes

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module sends a request and
+checks for error codes greater or = to 400
+"""
+import sys
+import requests
+
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <url>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+
+response = requests.get(url)
+
+if response.status_codes >= 400:
+    print(f"Error code: {response.status_codes}")
+else:
+    print(response)
