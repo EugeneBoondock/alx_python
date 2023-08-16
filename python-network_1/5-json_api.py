4bf34a63a95ba094bba06ea7d39b58f5fd6113ab
Eugene Ncube <philosncube@gmail.com>
2023-08-16 18:55:47 +0200

    fix

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -29,8 +29,8 @@ id = response.headers.get('X-Request-Id')
 
 try:
     json_response = response.json()
-    if isinstance(json_response, dict) and "id" in json_response and "name" in json_response:
-        print(f"[{id}] [{json_response['id']}] {json_response['name']}")
+    if json_response:
+        print("[{}] {}".format(json_response['id'], json_response['name']))
     else:
         print("No result")
 except ValueError:
