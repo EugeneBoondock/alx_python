2e35500dbf198ee7b89b052f8bae7421dc85e19d
Eugene Ncube <philosncube@gmail.com>
2023-08-16 18:22:01 +0200

    json response

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module does multiple things
+just read the code
+"""
+import sys
+import requests
+
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <url> <char>")
+    sys.exit(1)
+
+sys.argv[1] = 'http://0.0.0.0:5000/search_user'
+url = sys.argv[1]
+q = sys.argv[2]
+
+q = str(q)
+
+data = {'q': q}
+
+if len(q) != 1:
+    sys.exit(1)
+
+if q == 0:
+    q=""
+
+response = requests.post(url, data={'q': q})
+
+try:
+    json_response = response.json()
+    if json_response:
+        print(f"[{response.headers.get('X-Request-Id')}] {json_response}")
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
