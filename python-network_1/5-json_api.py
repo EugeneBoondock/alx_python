f134ba2d3d90489855ae7b7319e87ece846ffbbc
Eugene Ncube <philosncube@gmail.com>
2023-08-16 18:42:40 +0200

    fix

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -30,7 +30,7 @@ id = response.headers.get('X-Request-Id')
 try:
     json_response = response.json()
     if json_response:
-        print(f"[{id}] {json_response}")
+        print(f"[{id}] {json_response['id']} {json_response['name']}")
     else:
         print("No result")
 except ValueError:
