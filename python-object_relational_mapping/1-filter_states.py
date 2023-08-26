2f05038ace9247ff8e0599be3fce15ad92bea8a8
Eugene Ncube <philosncube@gmail.com>
2023-08-27 00:44:01 +0200

    hotfix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+Lists all states with a name starting with N 
+(upper N) from the database hbtn_0e_0_usa.
 """
+
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        user=sys.argv[1], 
-        passwd=sys.argv[2], 
+        user=sys.argv[1],
+        passwd=sys.argv[2],
         db=sys.argv[3])
 
     cursor = db.cursor()
     cursor.execute("""
-                   SELECT * FROM states 
+                   SELECT * FROM states
                    WHERE name LIKE BINARY 'N%'
                    ORDER BY id ASC
                    """)
