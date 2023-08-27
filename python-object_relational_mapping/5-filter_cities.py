7a031da86b0cb5c6b26175577634470831d409d6
Eugene Ncube <philosncube@gmail.com>
2023-08-27 17:03:30 +0200

    filter

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+"""
+Script that lists all states starting with cap N
+"""
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        print("""Usage: {} needs
+              <username>
+              <password> and
+              <database_name""".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    query = """
+            SELECT cities.id, cities.name, states.name
+            FROM cities WHERE states.name == %s
+            JOIN  states ON cities.state_id = states.id
+            ORDER BY id ASC"""
+
+    cursor.execute(query, (state_name + '%',))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
