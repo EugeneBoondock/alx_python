e128ca77e6f82743ebdece28a84b10deb1d582fe
Eugene Ncube <philosncube@gmail.com>
2023-08-27 17:58:49 +0200

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Start link class to table in database
+"""
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,15 +11,25 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    state_name = Column(String(250))
+    name = Column(String(128), nullable=False)
 
-if len(sys.argv) != 4:
-    print("Usage: {}  enter <username> <password> <database_name>")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("""Usage: {} 
+              <username>
+              <password>
+              <database_name>
+              """.format(sys.argv[0]))
+        sys.exit(1)
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-engine = create_engine(f"sqlite://{username}:{password}@localhost/3306/{database}")
-Base.metadata.createall(engine)
+    engine = create_engine('''mysql+mysqldb:
+                           //{}:{}@localhost/{}'''
+                           .format(username, 
+                                   password, 
+                                   database), 
+                                   pool_pre_ping=True)
+    Base.metadata.create_all(engine)
