031b455a6c4832bfb27ee269f4b7b2d3946ce122
Eugene Ncube <philosncube@gmail.com>
2023-08-27 17:12:15 +0200

    hotfix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     query = """
-            SELECT cities.id, cities.name, states.name
+            SELECT cities.name
             FROM cities
             JOIN states ON cities.state_id = states.id
             WHERE states.name LIKE %s
