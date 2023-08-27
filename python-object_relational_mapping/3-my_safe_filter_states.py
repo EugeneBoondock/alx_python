9a1c4cef317bd506c434531fd8d6600415c89271
Eugene Ncube <philosncube@gmail.com>
2023-08-27 16:26:14 +0200

    hotfix

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,48 +2,45 @@
 
 import MySQLdb
 import sys
+
 """
 Script that connects via MySQLdb
 avoids SQL injection
 """
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("""Usage: Enter username:{}
-            password:{}
-            db_name:{}
-            state_name:{}
-            """.format(sys.argv[1],
-                        sys.argv[2],
-                        sys.argv[3],
-                        sys.argv[4]))
-        
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
+        print("""Usage: Enter username:{} 
+              password:{} 
+              db_name:{} 
+              
+              state_name:{}""".format(
+            sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3]))
+        sys.exit(1)
 
-        db = MySQLdb.connect(
-            host = "localhost",
-            user = username,
-            passwd = password,
-            port = 3306,
-            db = database
-        )
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-        query = """SELECT * FROM states
-                WHERE name LIKE BINARY {} %s
-                ORDER BY state_id ASC
-                """.format(state_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        port=3306,
+        db=database
+    )
 
-        cursor= db.cursor()
-        cursor.execute(query)
-        db.commit()
+    query = """SELECT * FROM states
+               WHERE name LIKE BINARY %s
+               ORDER BY id ASC"""
 
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+    cursor = db.cursor()
+    cursor.execute(query, (state_name + '%',))
+    rows = cursor.fetchall()
 
-        cursor.close()
-        db.close()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
