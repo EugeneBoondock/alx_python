c866d6f35e75df2befbeeceb40bfc23072042838
Eugene Ncube <philosncube@gmail.com>
2023-08-27 18:01:05 +0200

    hotfix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""
-Start link class to table in database
-"""
+
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+"""
+Start link class to table in database
+"""
 
 Base = declarative_base()
 
 class State(Base):
+    """
+    Class uses sqlalchemy and inherits Base
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
