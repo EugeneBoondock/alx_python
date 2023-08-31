633335079c2f06fb856c5c699c7296cb9d8907ed
Eugene Ncube <philosncube@gmail.com>
2023-08-31 22:08:34 +0200

    hbnb

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,27 @@
+"""
+documented again and again
+"""
+from flask import Flask
+"""
+This is a flask web application
+"""
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    This function returns a string "Hello BNB!" when the root URL is accessed.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
