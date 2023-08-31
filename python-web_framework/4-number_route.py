a892265ebf3cd2c370198bc47e7f06792c827ef3
Eugene Ncube <philosncube@gmail.com>
2023-08-31 23:47:44 +0200

    hotfix

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -48,8 +48,10 @@ def number(n):
     """
     if n.isdigit():
         return f"{n} is a number"
-    else:
-        abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "404", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
