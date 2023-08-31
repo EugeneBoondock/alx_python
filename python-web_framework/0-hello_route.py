4f4c95c92955ffd3c844aef59e7fd5fc22ad9401
Eugene Ncube <philosncube@gmail.com>
2023-08-31 21:57:06 +0200

    fix

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -3,12 +3,14 @@ from flask import Flask
 This is a flask web application
 """
 
-
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def app():
-    return ("Hello BNB!")
+    """
+    This function returns a string "Hello BNB!" when the root URL is accessed.
+    """
+    return ("Hello HBNB!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
