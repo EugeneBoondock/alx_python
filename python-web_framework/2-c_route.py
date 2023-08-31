a509ee5d2ad689daa0290db3ae383514ad03082a
Eugene Ncube <philosncube@gmail.com>
2023-08-31 22:48:04 +0200

    fix

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -28,9 +28,7 @@ def c_is_fun(text):
     """
     This function returns a string "HBNB" when the URL /hbnb is accessed
     """
-    for _ in range(text):
-        if _ == '_':
-            print(" ")
+    text = text.replace("_", " ")
     return f"C {text}"
 
 if __name__ == '__main__':
