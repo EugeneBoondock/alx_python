3e9af9df35403383ed6102f5a9544953d194fd52
Eugene Ncube <philosncube@gmail.com>
2023-08-31 23:02:11 +0200

    fix

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -32,11 +32,10 @@ def c_is_fun(text):
     return f"C {text}"
 
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python(text="is cool"):
     """
     This function returns a string "HBNB" when the URL /hbnb is accessed
     """
-    text = "is cool"
     text = text.replace("_", " ")
     return f"Python {text}"
 
