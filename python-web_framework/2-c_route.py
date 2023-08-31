bce7c607ab1d365c80e8cb5febc50a0e9f5926ef
Eugene Ncube <philosncube@gmail.com>
2023-08-31 22:19:24 +0200

    fix

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,37 @@
+"""
+documented again and again
+"""
+from flask import Flask
+"""
+This is a flask web application
+"""
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    This function returns a string "Hello BNB!" when the root URL is accessed.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    for _ in text:
+        print(" ")
+
+    return f"C {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
