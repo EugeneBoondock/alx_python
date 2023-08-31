e0b0f48e17fdacfd2d3746456c3e23e1d2e41a5e
Eugene Ncube <philosncube@gmail.com>
2023-08-31 23:28:51 +0200

    is type

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,52 @@
+"""
+documented again and again
+"""
+from flask import Flask
+"""
+This is a flask web application
+"""
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    This function returns a string "Hello BNB!" when the root URL is accessed.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """
+    This function returns a string "HBNB" when the URL /hbnb is accessed
+    """
+    if n is type(int):
+        return "n is a number"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
