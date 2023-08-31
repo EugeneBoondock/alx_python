bf48bc90a6de910a18348b3537c8cafb0dcdf49a
Eugene Ncube <philosncube@gmail.com>
2023-08-31 23:23:53 +0200

    hotfix

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -31,6 +31,7 @@ def c_is_fun(text):
     text = text.replace("_", " ")
     return f"C {text}"
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     """
