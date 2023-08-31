a1f4c1f51ea4f3cf0fba3c265847073a91612bf9
EugeneBoondock <philosncube@gmail.com>
2023-08-31 22:47:04 +0000

    Hotfix

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -47,15 +47,12 @@ def number(n):
     else:
         abort(404)
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """
     Renders an HTML page that displays the number n.
     """
-    if n.isdigit():
-        return render_template('./templates/5-number.html', n=n)
-    else:
-        abort(404)
+    return render_template('./templates/5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
