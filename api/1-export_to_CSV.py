caa36954736ab07c809026fc4ad983197ea1281a
Eugene Ncube <philosncube@gmail.com>
2023-10-11 17:27:59 +0200

    hefghd

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python3
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress and exports it to CSV.
+"""
+
 import csv
 import requests
+from sys import argv
 
-def export_to_csv(user_id):
+if __name__ == '__main':
+    user_id = argv[1]
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     response = requests.get(url)
     user_name = response.json().get('name')
@@ -10,18 +18,20 @@ def export_to_csv(user_id):
     response = requests.get(url)
     todos = response.json()
 
-    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-        total_tasks = len(todos)
-        done_tasks = sum([1 for todo in todos if todo['completed']])
-        writer.writerow([user_id, user_name, done_tasks, total_tasks])
-        for todo in todos:
-            if todo['completed']:
-                writer.writerow([user_id, user_name, 'True', todo['title']])
-            else:
-                writer.writerow([user_id, user_name, 'False', todo['title']])
+    total_tasks = len(todos)
+    done_tasks = sum(1 for todo in todos if todo['completed'])
 
-if __name__ == '__main__':
-    user_id = argv[1]
-    export_to_csv(user_id)
+    # Prepare the CSV data
+    csv_data = []
+    for todo in todos:
+        csv_data.append([user_id, user_name, str(todo['completed']), todo['title']])
+
+    # Write the data to a CSV file
+    filename = f'{user_id}.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csv_writer.writerows(csv_data)
+
+    print(f"Employee {user_name} is done with tasks({done_tasks}/{total_tasks}).")
+    print(f"Data exported to {filename}")
