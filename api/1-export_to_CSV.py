f91784ad5f6b51a292ad8e81d54253be2aae5427
Eugene Ncube <philosncube@gmail.com>
2023-10-11 17:47:34 +0200

    done

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,45 +1,37 @@
-import csv 
+import csv
 import requests
-from sys import argv
-from typing import List, Any
+import sys
+
+def fetch_employee_data(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/users/{employee_id}/todos"
+    
+    employee_response = requests.get(employee_url)
+    todos_response = requests.get(todos_url)
+    
+    if employee_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        sys.exit(1)
+    
+    employee_data = employee_response.json()
+    todos_data = todos_response.json()
+    return employee_data, todos_data
+
+def export_to_csv(employee_id, username, todos_data):
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode='w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for todo in todos_data:
+            csv_writer.writerow([employee_id, username, todo['completed'], todo['title']])
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    else:
-        employee_id = int(argv[1])
-        user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-        todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-        user_response = requests.get(user_url)
-        todo_response = requests.get(todo_url)
-
-        if user_response.status_code != 200:
-            print("User not found")
-        elif todo_response.status_code != 200:
-            print("TODO data not found")
-        else:
-            user_data = user_response.json()
-            todo_data = todo_response.json()
-
-            completed_tasks: List[Any] = [
-                task for task in todo_data if task['completed']]
-            total_tasks = len(todo_data)
-
-            print(
-                f"Employee {user_data['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-            for task in completed_tasks:
-                print(f"\t {task['title']}")
-
-            # Export data to CSV
-            csv_file_name = f"{employee_id}.csv"
-            with open(csv_file_name, mode='w', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(
-                    ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-                for task in todo_data:
-                    csv_writer.writerow(
-                        [user_data['id'], user_data['username'], task['completed'], task['title']])
-
-            print(f"Data exported to {csv_file_name}")
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python script_name.py <employee_id>")
+        sys.exit(1)
+    
+    employee_id = int(sys.argv[1])
+    employee_data, todos_data = fetch_employee_data(employee_id)
+    username = employee_data.get('username', 'Unknown User')
+    export_to_csv(employee_id, username, todos_data)
