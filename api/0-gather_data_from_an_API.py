cc774577d09137b0b8c66b2f691a245a59bac091
Eugene Ncube <philosncube@gmail.com>
2023-10-11 16:48:41 +0200

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,53 +1,36 @@
 import requests
 
-def get_employee_todo_list_progress(employee_id):
-  """Returns information about an employee's TODO list progress.
+def get_employee_todos(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch data for employee {employee_id}")
+        return None
 
-  Args:
-    employee_id: The ID of the employee.
+def display_todo_progress(todos):
+    completed_todos = [todo for todo in todos if todo["completed"]]
+    total_todos = len(todos)
+    completed_todos_count = len(completed_todos)
+    employee_name = get_employee_name(todos[0]["userId"])
+    print(f"Employee {employee_name} is done with tasks({completed_todos_count}/{total_todos}):")
+    for todo in completed_todos:
+        print(f"\t{todo['title']}")
 
-  Returns:
-    A string containing the employee's name, the number of completed tasks,
-    the total number of tasks, and a list of the titles of completed tasks.
-  """
-
-  # Get the employee details.
-  employee_details_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-  employee_details_response = requests.get(employee_details_url)
-  employee_details = employee_details_response.json()
-
-  # Get the TODO list for the employee.
-  employee_todo_list_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-  employee_todo_list_response = requests.get(employee_todo_list_url)
-  employee_todo_list = employee_todo_list_response.json()
-
-  # Calculate the number of completed tasks.
-  number_of_completed_tasks = 0
-  for task in employee_todo_list:
-    if task["completed"]:
-      number_of_completed_tasks += 1
-
-  # Calculate the total number of tasks.
-  total_number_of_tasks = len(employee_todo_list)
-
-  # Get a list of the titles of completed tasks.
-  completed_task_titles = []
-  for task in employee_todo_list:
-    if task["completed"]:
-      completed_task_titles.append(task["title"])
-
-  # Format the output string.
-  output_string = f"Employee {employee_details['name']} is done with tasks({number_of_completed_tasks}/{total_number_of_tasks}):\n"
-  for task_title in completed_task_titles:
-    output_string += f"    {task_title}\n"
-
-  return output_string
-
+def get_employee_name(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()["name"]
+    else:
+        print(f"Error: Unable to fetch data for employee {employee_id}")
+        return None
 
 if __name__ == "__main__":
-  # Get the employee ID from the user.
-  employee_id = int(input("Enter the employee ID: "))
-
-  # Get the employee's TODO list progress and display it to the user.
-  employee_todo_list_progress = get_employee_todo_list_progress(employee_id)
-  print(employee_todo_list_progress)
+    employee_id = int(input("Enter employee ID: "))
+    todos = get_employee_todos(employee_id)
+    if todos:
+        display_todo_progress(todos)
+    else:
+        print("No data found for employee.")
