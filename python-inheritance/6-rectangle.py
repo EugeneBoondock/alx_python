cec0bc23c2dcba884a0e514f9b936224af5879be
Eugene Ncube <philosncube@gmail.com>
2023-10-11 18:28:44 +0200

    fix

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
-Contains a function for checking if an object
-is an instance of a class that inherited from a specified class.
+This module contains the Rectangle class that inherits from BaseGeometry.
 """
 
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-def inherits_from(obj, a_class):
+
+class Rectangle(BaseGeometry):
     """
-    Check if an object is an instance of a
-    class that inherited from a specified class.
-
-    Args:
-        obj: The object to be checked.
-        a_class: The class to compare against.
-
-    Returns:
-        True if the object is an instance of a class
-        that inherited from the specified class; otherwise False.
+    A class named Rectangle that inherits from BaseGeometry.
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        # Validate and set width and height using the integer_validator method
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        # Set width and height as private attributes
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
