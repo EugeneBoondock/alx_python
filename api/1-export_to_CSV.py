394b32938c87c1785eeb9a7bd118e85fa7f8d2a0
Eugene Ncube <philosncube@gmail.com>
2023-10-11 17:38:53 +0200

    pfeeeww

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,15 +4,24 @@ import csv
 import requests
 from sys import argv
 
+def user_info(user_id):
+    file_name = '{}.csv'.format(user_id)
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            # do something with each row
+            pass 
+
 if __name__ == '__main__':
 
     user_id = argv[1]
     
+    # Write CSV file
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     response = requests.get(url)
     user_name = response.json().get('name')
 
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id) 
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
     response = requests.get(url)
     todos = response.json()
 
@@ -22,3 +31,6 @@ if __name__ == '__main__':
         for todo in todos:
             row = [user_id, user_name, str(todo['completed']), todo['title']]
             writer.writerow(row)
+
+    # Now read CSV file
+    user_info(user_id)
