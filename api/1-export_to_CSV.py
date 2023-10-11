8c6d5c2542bb8eef51d782f82f7d0de93a71cf34
Eugene Ncube <philosncube@gmail.com>
2023-10-11 19:38:24 +0200

    fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,13 +7,27 @@ import json
 import requests
 from sys import argv
 
-
-if __name__ == "__main__":
+def export_data(user_id):
     """
         Request user info by employee ID
     """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    try:
+        request_employee = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+        request_employee.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print ("HTTP Error:",errh)
+        return
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting:",errc)
+        return
+    except requests.exceptions.Timeout as errt:
+        print ("Timeout Error:",errt)
+        return
+    except requests.exceptions.RequestException as err:
+        print ("Something went wrong",err)
+        return
+
     """
         Convert json to dictionary
     """
@@ -26,8 +40,23 @@ if __name__ == "__main__":
     """
         Request user's TODO list
     """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    try:
+        request_todos = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
+        request_todos.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print ("HTTP Error:",errh)
+        return
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting:",errc)
+        return
+    except requests.exceptions.Timeout as errt:
+        print ("Timeout Error:",errt)
+        return
+    except requests.exceptions.RequestException as err:
+        print ("Something went wrong",err)
+        return
+
     """
         Dictionary to store task status(completed) in boolean format
     """
@@ -45,9 +74,13 @@ if __name__ == "__main__":
     """
         Export to CSV
     """
-    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
+    with open('{}.csv'.format(user_id), mode='w', newline='') as file:
         file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
         for k, v in tasks.items():
-            file_editor.writerow([argv[1], username, v, k])
+            file_editor.writerow([user_id, username, v, k])
 
-    print('Data exported to {}.csv'.format(argv[1]))
+if __name__ == "__main__":
+   if len(argv) > 1:
+       export_data(argv[1])
+   else:
+       print("Please provide a user id as an argument.")
