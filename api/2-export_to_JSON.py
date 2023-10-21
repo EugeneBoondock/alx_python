a001b9e3d4c3bd3a51de1509c1012bf6e2f08510
Eugene Ncube <philosncube@gmail.com>
2023-10-22 01:07:44 +0200

    test

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,52 +1,28 @@
 #!/usr/bin/python3
 """
-    python script that exports data in the JSON format
+    Uses the fake API to get all employers and their todos and export as json
 """
 import json
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    """
-        request user info by employee ID
-    """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-    """
-        convert json to dictionary
-    """
-    user = json.loads(request_employee.text)
-    """
-        extract username
-    """
-    username = user.get("username")
-
-    """
-        request user's TODO list
-    """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    """
-        list to store task status(completed) in dictionary format
-    """
-    tasks = []
-    """
-        convert json to list of dictionaries
-    """
-    user_todos = json.loads(request_todos.text)
-    """
-        loop through dictionary & get completed tasks
-    """
-    for dictionary in user_todos:
-        task = {}
-        task["task"] = dictionary.get("title")
-        task["completed"] = dictionary.get("completed")
-        task["username"] = username
-        tasks.append(task)
-
-    """
-        export to JSON
-    """
-    with open('{}.json'.format(argv[1]), 'w') as json_file:
-        json.dump({argv[1]: tasks}, json_file)
+    url_employ = "https://jsonplaceholder.typicode.com/users"
+    r_employs = requests.get(url_employ).json()
+    report = {}
+    for employ in r_employs:
+        id_em = employ.get("id")
+        url_todos = url_employ + "/{}/todos".format(id_em)
+        r_todos = requests.get(url_todos).json()
+        username = employ.get("username")
+        total_num_task = r_todos
+        list_dict_report = []
+        for task in total_num_task:
+            id_report = {}
+            id_report["username"] = str(username)
+            id_report["completed"] = task.get("completed")
+            id_report["task"] = str(task.get("title"))
+            list_dict_report.append(id_report)
+        report[id_em] = list_dict_report
+    with open("todo_all_employees.json", "w") as fjson:
+        fjson.write(json.dumps(report))
