c5cb663629c2092f8ca546cd9f51be0eef43bdc4
Eugene Ncube <philosncube@gmail.com>
2023-10-22 01:21:53 +0200

    fix

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
-"""
-    python script that exports data in the JSON format
-"""
+"""Returns to-do list information for a given employee ID."""
 import json
 import requests
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    """
-        request user info by employee ID
-    """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-    """
-        convert json to dictionary
-    """
-    user = json.loads(request_employee.text)
-    """
-        extract username
-    """
-    username = user.get("username")
 
-    """
-        request user's TODO list
-    """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    """
-        list to store task status(completed) in dictionary format
-    """
-    tasks = []
-    """
-        convert json to list of dictionaries
-    """
-    user_todos = json.loads(request_todos.text)
-    """
-        loop through dictionary & get completed tasks
-    """
-    for dictionary in user_todos:
-        task = {}
-        task["task"] = dictionary.get("title")
-        task["completed"] = dictionary.get("completed")
-        task["username"] = username
-        tasks.append(task)
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = sys.argv[1]
 
-    """
-        export to JSON with indentation
-    """
-    with open('{}.json'.format(argv[1]), 'w') as json_file:
-        json.dump({argv[1]: tasks}, json_file, indent=2)
+    user = requests.get("{}/users/{}".format(url, user_id)).json()
+    todos = requests.get(url + "/todos", params={"userId": user_id}).json()
+
+    username = user.get('username')
+
+    dict = {}
+    data = []
+
+    for todo in todos:
+        data.append({'task': todo.get('title'),
+                    'completed': todo.get('completed'), 'username': username})
+
+    dict[user_id] = data
+
+    filename = user_id + ".json"
+    with open(filename, "w", encoding="utf-8") as json_file:
+        json_text = json.dumps(dict)
+        json_file.write(json_text)
