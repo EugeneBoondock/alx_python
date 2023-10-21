db8b7b0d6078acded059d59d823fd2dc1555ab01
Eugene Ncube <philosncube@gmail.com>
2023-10-21 20:28:34 +0200

    fix

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def fibonacci_sequence(n):
+    if n <= 0:
+        return []
     fib_sequence = [0, 1]
 
     while len(fib_sequence) < n:
         next_fib = fib_sequence[-1] + fib_sequence[-2]
         fib_sequence.append(next_fib)
 
-    return fib_sequence
+    return fib_sequence[:n]
