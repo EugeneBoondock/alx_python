57f51cd88aa5abeb19a0afb3ff3c6443bfc9781d
Eugene Ncube <philosncube@gmail.com>
2023-10-21 20:35:41 +0200

    fix

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,5 @@
+output = ""
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j:02}" if i != 0 else f"{i}{j}", end=", " if i != 8 else "\n")
+        output += "{:02d}, ".format(i * 10 + j) if i != 0 else "{:d}{:d}, ".format(i, j)
+print(output[:-2])
