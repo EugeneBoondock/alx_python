b04e70835fa0e20bb4ff9dc690d143944cea38d9
Eugene Ncube <philosncube@gmail.com>
2023-10-21 20:34:35 +0200

    fix

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,3 @@
-combinations = []
 for i in range(10):
     for j in range(i + 1, 10):
-        combinations.append("{:02}".format(i * 10 + j))
-print(", ".join(combinations))
+        print(f"{i}{j:02}" if i != 0 else f"{i}{j}", end=", " if i != 8 else "\n")
