a69c45a06d5b4a9b3c940691edb17b877f1b26e4
Eugene Ncube <philosncube@gmail.com>
2023-10-22 01:13:49 +0200

    fix

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-    Uses the fake API to get all employers and their todos and export as json
-"""
-import json
-import requests
+"""fetches information from JSONplaceholder API and exports to JSON"""
+
+from json import dump
+from requests import get
 from sys import argv
 
+
 if __name__ == "__main__":
-    url_employ = "https://jsonplaceholder.typicode.com/users"
-    r_employs = requests.get(url_employ).json()
-    report = {}
-    for employ in r_employs:
-        id_em = employ.get("id")
-        url_todos = url_employ + "/{}/todos".format(id_em)
-        r_todos = requests.get(url_todos).json()
-        username = employ.get("username")
-        total_num_task = r_todos
-        list_dict_report = []
-        for task in total_num_task:
-            id_report = {}
-            id_report["username"] = str(username)
-            id_report["completed"] = task.get("completed")
-            id_report["task"] = str(task.get("title"))
-            list_dict_report.append(id_report)
-        report[id_em] = list_dict_report
-    with open("todo_all_employees.json", "w") as fjson:
-        fjson.write(json.dumps(report))
+    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        argv[1])
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
+
+    todo_list = []
+    for todo in todo_result:
+        todo_dict = {}
+        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
+            "completed"), "username": name_result.get("username")})
+        todo_list.append(todo_dict)
+
+    with open("{}.json".format(argv[1]), 'w') as f:
+        dump({argv[1]: todo_list}, f)
