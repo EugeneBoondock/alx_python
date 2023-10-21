2d9152756c3905479a7c9795d5567816634e6afb
Eugene Ncube <philosncube@gmail.com>
2023-10-22 00:32:18 +0200

    fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,8 +4,8 @@ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress and exports it to a CSV file.
 """
 
-import requests
 import csv
+import requests
 from sys import argv
 
 
