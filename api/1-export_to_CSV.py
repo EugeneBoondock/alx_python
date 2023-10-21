fa621229b9a708f10a9ab7f8192589267111b55a
Eugene Ncube <philosncube@gmail.com>
2023-10-22 00:09:49 +0200

    fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,37 +1,29 @@
+#!/usr/bin/python3
+"""
+Python script that exports data in the CSV format.
+"""
 import csv
 import requests
-import sys
+from sys import argv
 
-if len(sys.argv) != 2:
-    sys.exit(1)
+def export_data(user_id):
+    """
+    Request user info by employee ID.
+    """
+    try:
+        user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
+        todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)).json()
+    except requests.exceptions.RequestException as e:
+        print("Something went wrong:", e)
+        return
 
-employee_id = int(sys.argv[1])
+    username = user.get("username")
 
-employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            taskwriter.writerow([user_id, username, task['completed'], task['title']])
 
-employee_response = requests.get(employee_url)
-todos_response = requests.get(todos_url)
-
-if employee_response.status_code != 200 or todos_response.status_code != 200:
-    sys.exit(1)
-
-employee_data = employee_response.json()
-todo_data = todos_response.json()
-employee_name = employee_data.get("name", "unknown employee")
-employee_username = employee_data.get("username", "unkown employee")
-
-csv_filename = f"{employee_id}.csv"
-
-with open(csv_filename, mode="w", newline="") as csv_file:
-    csv_writer = csv.writer(csv_file)
-
-    csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-for task in todo_data:
-        task_completed_status = "Completed" if task["completed"] else "Not Completed"
-        csv_writer.writerow([employee_id, employee_username, task_completed_status, task["title"]])
-
-
-with open(csv_filename, 'r') as f:
-     pass
+if __name__ == "__main__":
+    if len(argv) > 1:
+        export_data(argv[1])
