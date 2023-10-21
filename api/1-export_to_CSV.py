21f7f7827fbfef769020e84809fa9a0311ef037e
Eugene Ncube <philosncube@gmail.com>
2023-10-22 00:34:44 +0200

    fix

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """
 Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress and exports it to a CSV file.
+returns information about his/her TODO list progress.
 """
 
 import csv
 import requests
 from sys import argv
-
 
 if __name__ == '__main__':
     user_id = argv[1]
@@ -19,13 +18,7 @@ if __name__ == '__main__':
     response = requests.get(url)
     todos = response.json()
 
-    total_tasks = len(todos)
-    done_tasks = sum([1 for todo in todos if todo['completed']])
-
-    with open(f'{user_id}.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+    with open('{}.csv'.format(user_id), 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for todo in todos:
-            writer.writerow([user_id, user_name, str(todo['completed']), todo['title']])
-
-    print(f"Data has been exported to {user_id}.csv successfully.")
+            writer.writerow([user_id, user_name, todo['completed'], todo['title']])
